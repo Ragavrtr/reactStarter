cda126e7972a638498181254a3cfb3d6d3272e76
Ragavrtr <ragav.subramanian03@gmail.com>
2024-07-01 20:21:17 +0530

    Node with multiple Handles

--- my-react-flow-app/src/App.jsx
+++ my-react-flow-app/src/App.jsx
@@ -23,6 +23,8 @@ const initialNodes = [
   { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
   { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
   { id: 'node-3', type: 'textUpdater', position: { x: 0, y: 200 }, data: { value: 123 } },
+  { id: '4', type:"output", position: { x: 0, y: 300 }, data: { label: '2' } },
+
 ];
 
 // we define the nodeTypes outside of the component to prevent re-renderings
@@ -30,7 +32,11 @@ const initialNodes = [
 const nodeTypes = { textUpdater: TextUpdaterNode };
 
 
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+const initialEdges = [
+  { id: 'e1-2', source: '1', target: '2' },
+  { id: 'edge-1', source: 'node-3', target: '4', sourceHandle: 'a' },
+  { id: 'edge-2', source: 'node-3', target: '4', sourceHandle: 'b' },
+];
  
 export default function App() {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
