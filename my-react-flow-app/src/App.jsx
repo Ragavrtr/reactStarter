ffbe5b69e4fba59d27e6504bf611de1295cc16ec
Ragavrtr <ragav.subramanian03@gmail.com>
2024-07-01 21:05:48 +0530

    Custom Edge with delete button, addEdge - new edges will be animated

--- my-react-flow-app/src/App.jsx
+++ my-react-flow-app/src/App.jsx
@@ -3,6 +3,9 @@ import { applyEdgeChanges, applyNodeChanges } from 'reactflow';
 
 import TextUpdaterNode from './TextUpdaterNode.jsx';
 import './text-updater-node.css';
+import CounterNode from './CounterNode';
+
+import CustomEdge from './CustomEdge'
 
 const rfStyle = {
   backgroundColor: '#B8CEFF',
@@ -23,17 +26,21 @@ const initialNodes = [
   { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
   { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
   { id: 'node-3', type: 'textUpdater', position: { x: 0, y: 200 }, data: { value: 123 } },
-  { id: '4', type:"output", position: { x: 0, y: 300 }, data: { label: '2' } },
-
+  { id: '4', type:"output", position: { x: 0, y: 300 }, data: { label: '4' } },
+  { id: '5', type:"counterNode", position: { x: 0, y: 400 }, data: { label: '5' } },
 ];
 
 // we define the nodeTypes outside of the component to prevent re-renderings
 // you could also use useMemo inside the component
-const nodeTypes = { textUpdater: TextUpdaterNode };
+const nodeTypes = { textUpdater: TextUpdaterNode ,counterNode: CounterNode};
+
+const edgeTypes = {
+  'custom-edge': CustomEdge
+}
 
 
 const initialEdges = [
-  { id: 'e1-2', source: '1', target: '2' },
+  { id: 'e1-2', type:'custom-edge',source: '1', target: '2' },
   { id: 'edge-1', source: 'node-3', target: '4', sourceHandle: 'a' },
   { id: 'edge-2', source: 'node-3', target: '4', sourceHandle: 'b' },
 ];
@@ -43,7 +50,7 @@ export default function App() {
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
  
   const onConnect = useCallback(
-    (params) => setEdges((eds) => addEdge(params, eds)),
+    (params) => setEdges((eds) => addEdge({...params,animated:true}, eds)),
     [setEdges],
   );
  
@@ -56,6 +63,7 @@ export default function App() {
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
         nodeTypes={nodeTypes}
+        edgeTypes={edgeTypes}
         style={rfStyle}
       >
         <Controls />
---
+++ my-react-flow-app/src/CounterNode.tsx
@@ -0,0 +1,20 @@
+import { NodeProps } from 'reactflow';
+import { useState } from 'react';
+import React from 'react';
+ 
+export type CounterData = {
+  initialCount?: number;
+};
+ 
+export default function CounterNode(props: NodeProps<CounterData>) {
+  const [count, setCount] = useState(props.data?.initialCount ?? 0);
+ 
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button className="nodrag" onClick={() => setCount(count + 1)}>
+        Increment
+      </button>
+    </div>
+  );
+}
